5acfed790d982116a83230bd2b11130e6949beb2
TomasJaures <t.jaures01@ufromail.cl>
2026-05-12 01:21:14 -0400

    Cambio de carpetas

--- api/rua-api/src/main/java/com/group/rua/Controllers/HelloApplication.java
+++ api/rua-api/src/main/java/com/group/rua/Controllers/HelloApplication.java
@@ -1,13 +1,22 @@
 package com.group.rua.Controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
+@Controller
 public class HelloApplication {
     @GetMapping("/")
+    public String inicio(Model model) {
+        
+        model.addAttribute("mensaje", "Hola desde Thymeleaf");
+
+        return "index";
+    }
+
     String hello(){
         return "Hello World desde la WEB!";
     }
