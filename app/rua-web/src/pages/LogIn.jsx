6c11042eea6021f5c4dcc29be99c3820d2aa2eef
Alonso <alonsofariasravanal@gmail.com>
2026-05-16 14:07:28 -0400

    login signup y poco mas

---
+++ app/rua-web/src/pages/LogIn.jsx
@@ -0,0 +1,92 @@
+import RuaAside from "../components/rua-aside.jsx";
+import Card from "../components/card.jsx";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "../components/navbar.jsx";
+
+function LogIn() {
+
+  const [rol, setRol] = useState("docente");
+  const [correo, setCorreo] = useState("");
+  const [clave, setClave] = useState("");
+
+  function onConfirmarClick() {
+
+  console.log("ROL:", rol);
+
+  console.log("CORREO:", correo);
+
+  console.log("CLAVE:", clave);
+}
+
+  return (
+    <div className="pagina">
+      
+      <RuaAside>
+        Plataforma de registro de asistencia para la
+        Universidad de la Frontera.
+        Gestiona tu asistencia con facilidad.
+      </RuaAside>
+
+      {/* PANEL DERECHO */}
+      <main className="derecha">
+
+        <div className="barra-mobile">
+          <h1>RUA</h1>
+        </div>
+
+        <Card>
+
+          <h1>Log In</h1>
+
+          <p>Ingresa con tus credenciales institucionales</p>
+
+          {/* BOTONES */}
+          <div className="grupo-botones">
+
+            <button
+              onClick={() => setRol("estudiante")}
+              className={ rol === "estudiante" ? "activo" : "inactivo" }
+            >
+              Soy Alumno
+            </button>
+
+            <button
+              onClick={() => setRol("docente")}
+              className={ rol === "docente" ? "activo" : "inactivo" }
+            >
+              Soy Docente
+            </button>
+
+          </div>
+
+          {/* INPUTS */}
+          <label>CORREO INSTITUCIONAL</label>
+
+          <input
+            type="text"
+            placeholder={ rol === "estudiante" ? "ejemplo@ufromail.cl" : "ejemplo@ufrontera.cl" }
+            value={correo}
+
+            onChange={(e) => setCorreo(e.target.value)}
+          />
+
+          <label>CLAVE RUA</label>
+
+          <input type="password" placeholder="••••••••"
+          value={clave}
+
+          onChange={(e) => setClave(e.target.value)}
+          />
+
+          <button className="confirmar" onClick={onConfirmarClick}>Confirmar</button>
+
+          <hr />
+          <p className="signup">¿No te has registrado aún?<a href="/signup"> Sign Up</a></p>
+        </Card>
+      </main>
+    </div>
+  );
+}
+
+export default LogIn;
