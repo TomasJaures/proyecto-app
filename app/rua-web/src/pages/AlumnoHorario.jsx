d4e9184ad380a12c5306742e4178169cf9d99fa0
TomasJaures <t.jaures01@ufromail.cl>
2026-07-13 14:48:50 -0400

    feat(EJ 7): Cargar bloques en AlumnoHorario

--- app/rua-web/src/pages/AlumnoHorario.jsx
+++ app/rua-web/src/pages/AlumnoHorario.jsx
@@ -5,6 +5,7 @@ import Card from "../components/Card.jsx";
 import Schedule from "../components/Schedule.jsx";
 import { useAuth } from "../hooks/useAuth.js";
 import { calendarApi } from "../services/apiService.js";
+import { calendarMockApi } from "../services/mockServices.js";
 
 function AlumnoHorario() {
   const navigate = useNavigate();
@@ -17,46 +18,15 @@ function AlumnoHorario() {
   const [blocks, setBlocks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [loadError, setLoadError] = useState(null);
+
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchBlocks = async () => {
       try {
         setIsLoading(true);
         setLoadError(null);
-
-        const [blocksResponse, classesResponse] = await Promise.all([
-          calendarApi.getBlocks(calendarId),
-          calendarApi.getActualClasses(calendarId),
-        ]);
-
-        const blocks = blocksResponse.data;
-
-        const {
-          currentWeek,
-          classInfoDTOs,
-        } = classesResponse.data;
-
-        sessionStorage.setItem("currentWeek", currentWeek);
-
-        const updatedBlocks = blocks.map((block) => {
-          const info = classInfoDTOs.find(
-            (c) => c.blockId === block.id
-          );
-
-          if (!info) {
-            return block;
-          }
-
-          return {
-            ...block,
-            classId: info.classId,
-            isAnulled: info.isAnulled,
-            timeState: info.timeState,
-          };
-        });
-
-        setBlocks(updatedBlocks);
-        setClassInfo(classInfoDTOs);
-
+        const {data} = await calendarMockApi.getBlocks(calendarId);
+        //const { data } = await calendarApi.getBlocks(calendarId);
+        setBlocks(data);
       } catch (err) {
         setLoadError(err.message);
       } finally {
@@ -64,9 +34,7 @@ function AlumnoHorario() {
       }
     };
 
-    if (calendarId) {
-      fetchData();
-    }
+    if (calendarId) fetchBlocks();
   }, [calendarId]);
 
 
@@ -80,7 +48,7 @@ function AlumnoHorario() {
 
       <div className="alumno-contenido">
         <Card>
-          <h1>Hola,</h1>
+          <h1>Hola, {user?.name || "NoName"}</h1>
           <p>Estás registrado como Alumno.</p>
         </Card>
       </div>
